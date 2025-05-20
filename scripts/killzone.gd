d7f6ee5fd15394e4990044e149b7a46c2bf4a84d
Haadia <haadianadeem03@gmail.com>
2025-05-20 13:42:24 -0700

    undid checkpoint system

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,12 +1,6 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-var checkpoint_manager
-var player
-
-func _ready() -> void:
-	checkpoint_manager = get_parent().get_node("CheckpointManager")
-	player = get_parent().get_node("Player")
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
@@ -16,5 +10,4 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1.0
-	player.position = checkpoint_manager.last_location
 	get_tree().reload_current_scene()
