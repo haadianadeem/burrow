bfe9a5a9ab806c7dbf85d46f4c218f26e668b3aa
Haadia <haadianadeem03@gmail.com>
2025-05-22 17:15:15 -0700

    fixed objective dialgue issue

--- Resources/Quest/Objectives.gd
+++ Resources/Quest/Objectives.gd
@@ -10,7 +10,7 @@ class_name Objectives
 @export var target_id: String
 @export var target_type: String
 # talk_to objective
-@export var objective_dialog: String = ""
+@export var objective_dialogue: String = ""
 # collection objective
 @export var required_quantity: int = 0
 @export var collected_quantity: int = 0
