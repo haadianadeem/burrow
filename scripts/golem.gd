de70bb191a169482e718d9c6422767a5273557bf
Haadia <haadianadeem03@gmail.com>
2025-06-07 23:20:19 -0700

    fixed golem's direction, turning to face player, killzone movement

--- scripts/golem.gd
+++ scripts/golem.gd
@@ -9,9 +9,13 @@ var player_in_range: bool = false
 
 enum State { PATROLLING, ATTACKING }
 var state = State.PATROLLING
+var is_attacking := false
 
 func _ready():
 	start_position = position
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
+
+	
 	#$Killzone.monitoring = false
 	#$Killzone.disabled = true
 
@@ -27,35 +31,69 @@ func patrol(delta):
 	
 	if abs(position.x - start_position.x) >= patrol_distance:
 		direction *= -1
-		$AnimatedSprite2D.flip_h = direction > 0
+		scale.x = -1 if direction < 0 else 1
+		#$AnimatedSprite2D.flip_h = direction > 0
+		#$KillzoneFlipper.scale.x = -1 if direction > 0 else 1
+
 
 func _on_player_detection_body_entered(body):
 	if body.is_in_group("player") and state != State.ATTACKING:
 		player_in_range = true
+		turn_to_face(body.global_position)
 		attack()
+		
+func turn_to_face(player_pos: Vector2):
+	var delta_x = player_pos.x - global_position.x
+	if (delta_x < 0 and direction > 0) or (delta_x > 0 and direction < 0):
+		direction *= -1
+		scale.x = -1 if direction < 0 else 1
 
 func _on_player_detection_body_exited(body):
 	if body.is_in_group("player"):
 		player_in_range = false
 
 func attack():
+	if is_attacking:
+		return
+	
+	is_attacking = true
 	state = State.ATTACKING
 	$AnimatedSprite2D.play("attack")
-	$AnimationPlayer.play("attack")  # handles killzone activation/deactivation + return
+	$AnimationPlayer.play("attack")
+	
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack":
+		finish_attack()
 
-func activate_killzone():
-	print("killzone activated")
-	$Killzone.monitoring = true
-	$Killzone/CollisionShape2D.set_deferred("disabled", false)
-
-func deactivate_killzone():
-	print("killzone deactivated")
-	$Killzone.monitoring = false
-	$Killzone/CollisionShape2D.set_deferred("disabled", true)
-
-func return_to_patrol():
+func finish_attack():
+	is_attacking = false
+	# Don't stop animation manually; it has already finished
+	
 	if player_in_range:
-		attack()  # Repeat attack if player still in range
+		$AttackCooldownTimer.start()
 	else:
 		state = State.PATROLLING
 		$AnimatedSprite2D.play("walk")
+		
+	
+func activate_killzone():
+	$KillzoneFlipper/Killzone.monitoring = true
+	$KillzoneFlipper/Killzone/CollisionShape2D.set_deferred("disabled", false)
+	
+
+func deactivate_killzone():
+	print("killzone deactivated")
+	$KillzoneFlipper/Killzone.monitoring = false
+	$KillzoneFlipper/Killzone/CollisionShape2D.set_deferred("disabled", true)
+
+func return_to_patrol():
+	if player_in_range:
+		attack()  # Will only work if attack is marked finished
+	else:
+		state = State.PATROLLING
+		$AnimatedSprite2D.play("walk")
+
+
+func _on_attack_cooldown_timer_timeout() -> void:
+	if player_in_range and not is_attacking:
+		attack()
