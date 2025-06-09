beaa6a424e91ec7954a9ce2678ad6e194da8261e
Haadia <haadianadeem03@gmail.com>
2025-06-08 17:27:32 -0700

    added raycast nodes to golem.tscn

--- scripts/golem.gd
+++ scripts/golem.gd
@@ -13,11 +13,7 @@ var is_attacking := false
 
 func _ready():
 	start_position = position
-	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
-
-	
-	#$Killzone.monitoring = false
-	#$Killzone.disabled = true
+	$AnimationPlayer.stop()
 
 func _physics_process(delta):
 	if state == State.PATROLLING:
@@ -26,27 +22,22 @@ func _physics_process(delta):
 func patrol(delta):
 	if $AnimatedSprite2D.animation != "walk":
 		$AnimatedSprite2D.play("walk")
-		
-	position.x += direction * speed * delta
-	
-	if abs(position.x - start_position.x) >= patrol_distance:
+
+	# Wall or ledge detection
+	if $RayCast_Wall.is_colliding() or not $RayCast_Floor.is_colliding():
 		direction *= -1
 		scale.x = -1 if direction < 0 else 1
-		#$AnimatedSprite2D.flip_h = direction > 0
-		#$KillzoneFlipper.scale.x = -1 if direction > 0 else 1
+		$RayCast_Wall.position.x = abs($RayCast_Wall.position.x) * direction
+		$RayCast_Floor.position.x = abs($RayCast_Floor.position.x) * direction
 
+	# Move
+	position.x += direction * speed * delta
 
 func _on_player_detection_body_entered(body):
 	if body.is_in_group("player") and state != State.ATTACKING:
 		player_in_range = true
 		turn_to_face(body.global_position)
 		attack()
-		
-func turn_to_face(player_pos: Vector2):
-	var delta_x = player_pos.x - global_position.x
-	if (delta_x < 0 and direction > 0) or (delta_x > 0 and direction < 0):
-		direction *= -1
-		scale.x = -1 if direction < 0 else 1
 
 func _on_player_detection_body_exited(body):
 	if body.is_in_group("player"):
@@ -55,45 +46,34 @@ func _on_player_detection_body_exited(body):
 func attack():
 	if is_attacking:
 		return
-	
+
 	is_attacking = true
 	state = State.ATTACKING
 	$AnimatedSprite2D.play("attack")
 	$AnimationPlayer.play("attack")
-	
-func _on_animation_finished(anim_name: String) -> void:
-	if anim_name == "attack":
-		finish_attack()
 
 func finish_attack():
 	is_attacking = false
-	# Don't stop animation manually; it has already finished
-	
+
 	if player_in_range:
-		$AttackCooldownTimer.start()
+		attack()
 	else:
 		state = State.PATROLLING
 		$AnimatedSprite2D.play("walk")
-		
-	
+
+func turn_to_face(player_pos: Vector2):
+	var delta_x = player_pos.x - global_position.x
+	if (delta_x < 0 and direction > 0) or (delta_x > 0 and direction < 0):
+		direction *= -1
+		scale.x = -1 if direction < 0 else 1
+		$RayCast_Wall.position.x = abs($RayCast_Wall.position.x) * direction
+		$RayCast_Floor.position.x = abs($RayCast_Floor.position.x) * direction
+
 func activate_killzone():
 	$KillzoneFlipper/Killzone.monitoring = true
 	$KillzoneFlipper/Killzone/CollisionShape2D.set_deferred("disabled", false)
-	
 
 func deactivate_killzone():
 	print("killzone deactivated")
 	$KillzoneFlipper/Killzone.monitoring = false
 	$KillzoneFlipper/Killzone/CollisionShape2D.set_deferred("disabled", true)
-
-func return_to_patrol():
-	if player_in_range:
-		attack()  # Will only work if attack is marked finished
-	else:
-		state = State.PATROLLING
-		$AnimatedSprite2D.play("walk")
-
-
-func _on_attack_cooldown_timer_timeout() -> void:
-	if player_in_range and not is_attacking:
-		attack()
